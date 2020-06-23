9c18ce7026a51b2cc77bfe626e3f47e0cd91cc59
Norielle Cruz <mail@noriellecruz.com>
2020-06-23 12:15:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Zender API GET Wrapper
+
+![Status](https://img.shields.io/badge/status-released-blue?style=for-the-badge)
+![Version](https://img.shields.io/badge/version-1.0-lightgrey?style=for-the-badge)
+
+# Usage Guide
+
+This wrapper will allow sending sms and creating data to zender using pure GET requests.
+
+> This will work for /send and /create POST endpoints
+
+To use the wrapper simply replace "/api/" with "/getwrap/" on your request url, this guide assumes that you already uploaded this wrapper on your zender site.
+
+It is really recommended to only use this wrapper for personal or internal use only and not for you user consumption.
---
+++ system/controllers/getwrap.php
@@ -0,0 +1,281 @@
+<?php
+/**
+ * @controller API GET Wrapper
+ * @desc Wrapper for GET method requests
+ */
+
+class Getwrap_Controller extends MVC_Controller
+{
+	public function index()
+	{
+		$this->header->allow();
+
+        $type = $this->sanitize->string($this->url->segment(3));
+
+        $this->cache->container("system.settings");
+
+        if($this->cache->empty()):
+            $this->cache->setArray($this->system->getSystemSettings());
+        endif;
+
+        set_system($this->cache->getAll());
+
+		switch($type):
+			case "send":
+                $request = $this->sanitize->array($_GET);
+                $key = $this->sanitize->string($_GET["key"]);
+
+                if(!$this->sanitize->length($key, 5))
+                    response(400, "Invalid Request!");
+
+                $this->cache->container("api.keys");
+
+                if($this->cache->empty()):
+                    $this->cache->setArray($this->api->getKeys());
+                endif;
+
+                $keys = $this->cache->getAll();
+
+                if(!array_key_exists($key, $keys))
+                    response(401, "API key is not valid!");
+
+                $api = $keys[$key];
+
+                if(!isset($request["phone"], $request["message"]))
+                    response(400, "Invalid Request!");
+
+                try {
+                    $number = $this->phone->parse($request["phone"]);
+                } catch(Brick\PhoneNumber\PhoneNumberParseException $e) {
+                    response(400, $e->getMessage());
+                }
+
+                if (!$number->isValidNumber())
+                    response(400, "Invalid mobile number!");
+
+                if(!$number->getNumberType(Brick\PhoneNumber\PhoneNumberType::MOBILE))
+                    response(400, "Invalid mobile number!");
+
+                $request["phone"] = $number->format(Brick\PhoneNumber\PhoneNumberFormat::E164);
+
+                if(isset($request["sim"])):
+                    if(!$this->sanitize->isInt($request["sim"]))
+                        response(400, "Invalid Request!");
+                endif;
+
+                if(isset($request["priority"])):
+                    if(!$this->sanitize->isInt($request["priority"]))
+                        response(400, "Invalid Request!");
+                endif;
+
+                if(isset($request["device"])):
+                    if(!$this->sanitize->isInt($request["device"]))
+                        response(400, "Invalid Request!");
+                endif;
+
+                if(!$this->sanitize->length($request["message"], 5))
+                    response(400, "Message is too short!");
+
+                $this->cache->container("api.devices.{$api["hash"]}");
+
+                if($this->cache->empty()):
+                    $this->cache->setArray($this->api->getDevices($api["uid"]));
+                endif;
+
+                $devices = $this->cache->getAll();
+
+                if(!isset($request["device"])):
+                    foreach($devices as $device):
+                        $this->cache->container("gateway.{$device["did"]}.{$api["hash"]}");
+
+                        $usort[] = [
+                            "id" => $device["id"],
+                            "did" => $device["did"],
+                            "pending" => count($this->cache->getAll())
+                        ];
+                    endforeach;
+                    
+                    usort($usort, function($previous, $next) {
+                        return $previous["pending"] > $next["pending"] ? 1 : -1;
+                    });
+
+                    $did = $usort[0]["did"];
+                    $device = $usort[0]["id"];
+                else:
+                    if(!array_key_exists($request["device"], $devices)):
+                        response(400, "Device doesn't exist!");
+                    else:
+                        $did = $devices[$request["device"]]["did"];
+                        $device = $request["device"];
+                    endif;
+                endif;
+
+                $filtered = [
+                    "uid" => $api["uid"],
+                    "did" => $did,
+                    "sim" => (isset($request["sim"]) ? ($request["sim"] < 1 ? 0 : 1) : 0),
+                    "phone" => $request["phone"],
+                    "message" => $request["message"],
+                    "status" => 0,
+                    "priority" => (isset($request["priority"]) ? ($request["priority"] < 1 ? 0 : 1) : 0),
+                    "api" => 1
+                ];
+
+                $create = $this->api->create("sent", $filtered);
+
+                if($create):
+                    $this->cache->container("gateway.{$did}.{$api["hash"]}");
+
+                    $this->cache->set($create, [
+                        "api" => (boolean) 1,
+                        "sim" => $filtered["sim"],
+                        "device" => (int) $device,
+                        "phone" => $filtered["phone"],
+                        "message" => $filtered["message"],
+                        "priority" => (boolean) ($filtered["priority"] < 1 ? 0 : 1),
+                        "timestamp" => time()
+                    ]);
+
+                    $this->cache->container("messages.{$api["hash"]}");
+                    $this->cache->clear();
+
+                    response(200, "Message has been added to queue on {$devices[$device]["name"]}", [
+                        "api" => (boolean) 1,
+                        "sim" => $filtered["sim"],
+                        "device" => (int) $device,
+                        "phone" => $filtered["phone"],
+                        "message" => $filtered["message"],
+                        "priority" => (boolean) $filtered["priority"],
+                        "timestamp" => time()
+                    ]);
+                else:
+                    response(400, "Something went wrong!");
+                endif;
+		
+				break;
+			default:
+                $vars = [
+                    "site_url" => (system_protocol < 2 ? str_replace("//", "http://", site_url) : str_replace("//", "https://", site_url))
+                ];
+                
+				$this->smarty->display("_apidoc/layout.tpl", $vars);
+		endswitch;
+	}
+
+	public function create()
+	{
+		$this->header->allow();
+
+		$request = $this->sanitize->array($_GET);
+        $key = $this->sanitize->string($_GET["key"]);
+        $type = $this->sanitize->string($this->url->segment(4));
+
+        if(empty($key))
+            response(400, "Invalid Request!");
+
+        $this->cache->container("api.keys");
+
+        if($this->cache->empty()):
+            $this->cache->setArray($this->api->getKeys());
+        endif;
+
+        $keys = $this->cache->getAll();
+
+        if(!array_key_exists($key, $keys))
+            response(401, "API key is not valid!");
+
+        $api = $keys[$key];
+
+		switch($type):
+            case "contact":
+                if(!in_array("create_{$type}", $api["permissions"]))
+                    response(403, "Permission \"create_{$type}\" not granted!");
+
+                if(!isset($request["phone"], $request["name"], $request["group"]))
+                    response(400, "Invalid Request");
+
+                if(empty($request["name"]))
+                    response(400, "Contact name cannot be empty!");
+
+                try {
+                    $number = $this->phone->parse($request["phone"]);
+                } catch(Brick\PhoneNumber\PhoneNumberParseException $e) {
+                    response(400, $e->getMessage());
+                }
+
+                if (!$number->isValidNumber())
+                    response(400, "Invalid mobile number!");
+
+                if(!$number->getNumberType(Brick\PhoneNumber\PhoneNumberType::MOBILE))
+                    response(400, "Invalid mobile number!");
+
+                $request["phone"] = $number->format(Brick\PhoneNumber\PhoneNumberFormat::E164);
+
+                if(!$this->sanitize->isInt($request["group"]))
+                    response(400, "Invalid Request!");
+
+                $this->cache->container("api.groups.{$api["hash"]}");
+
+                if($this->cache->empty()):
+                    $this->cache->setArray($this->api->getGroups($api["uid"]));
+                endif;
+
+                if(!array_key_exists($request["group"], $this->cache->getAll()))
+                    response(400, "Invalid Request!");
+
+                $this->cache->container("api.contacts.{$api["hash"]}");
+
+                if($this->cache->empty()):
+                    $this->cache->setArray($this->api->getContacts($api["uid"]));
+                endif;
+
+                if(array_key_exists($request["phone"], $this->cache->getAll()))
+                    response(400, "Contact number already exist!");
+
+                $filtered = [
+                    "uid" => $api["uid"],
+                    "gid" => $request["group"],
+                    "phone" => $request["phone"],
+                    "name" => $request["name"]
+                ];
+
+                if($this->api->create("contacts", $filtered)):
+                    $this->cache->container("api.contacts.{$api["hash"]}");
+                    $this->cache->clear();
+
+                    response(200, "Contact saved successfully!");
+                else:
+                    response(400, "Something went wrong!");
+                endif;
+
+                break;
+            case "group":
+                if(!in_array("create_{$type}", $api["permissions"]))
+                    response(403, "Permission \"create_{$type}\" not granted!");
+
+                if(!isset($request["name"]))
+                    response(400, "Invalid Request!");
+
+                if(empty($request["name"]))
+                    response(400, "Group name cannot be empty!");
+
+                $filtered = [
+                    "uid" => $api["uid"],
+                    "name" => $request["name"]
+                ];
+
+                if($this->api->create("groups", $filtered)):
+                    $this->cache->container("api.groups.{$api["hash"]}");
+                    $this->cache->clear();
+
+                    response(200, "Contact group saved sauccessfully!");
+                else:
+                    response(400, "Something went wrong!");
+                endif;
+
+                break;
+			default:
+				response(400, "Invalid Request!");
+		endswitch;
+	}
+}
